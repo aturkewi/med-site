3e9da6240b6d65c951142b4e22acd8615660864a
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-07-14 19:07:49 -0400

    Remove change of rel images in config

--- config.rb
+++ config.rb
@@ -9,8 +9,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-set :relative_images, true
-
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
